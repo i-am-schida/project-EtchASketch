ff18efbd1d4a3857ebf25c63917f6a9f2b7b37e8
Sam Ichida <84211012+i-am-schida@users.noreply.github.com>
2023-10-06 17:15:16 -0700

    reset, eraser button added
    reset button sets all divs inside main container to its original background color. eraser button is set as a toggle. by default, hovering over main container would leave a different color trail, with eraser mode on, hovering over the trail would set the color back to original.

--- script.js
+++ script.js
@@ -1,11 +1,32 @@
 document.addEventListener("DOMContentLoaded", function () {
   const mainContainer = document.querySelector('.main-container');
+  const resetButton = document.querySelector('#reset-button');
+  const eraserButton = document.querySelector('#eraser-button');
+  let isErasing = false;
+
 
   for (let i = 0; i < 16; i++) { // for looping 'i' for rows, 'j' for columns
     for (let j = 0; j < 16; j++) {
       const gridItem = document.createElement('div');
       gridItem.classList.add('grid-item');
       mainContainer.appendChild(gridItem);
+
+      gridItem.addEventListener('mouseenter', function () {
+        if (isErasing) {
+          gridItem.style.backgroundColor = 'gray'; // erase by setting to gray
+        } else {
+          gridItem.style.backgroundColor = 'pink';
+        }
+      });
     }
   }
+  resetButton.addEventListener('click', function () {
+    const gridItems = mainContainer.querySelectorAll('.grid-item');
+    gridItems.forEach(function (gridItem) {
+      gridItem.style.backgroundColor = 'gray';
+    });
+  });
+  eraserButton.addEventListener('click', function () {
+    isErasing = !isErasing; // toggle eraser mode
+  });
 });
