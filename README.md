3a85836787b3b3bf2e20881e12b19e9da7bb058f
Sam Ichida <84211012+i-am-schida@users.noreply.github.com>
2023-10-06 20:13:13 -0700

    color mode button added
    added this button as default button, adjusted reset button to when clicked it defaults back to color mode.

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # project-EtchASketch
- 
+
+https://i-am-schida.github.io/project-EtchASketch/
--- script.js
+++ script.js
@@ -2,31 +2,91 @@ document.addEventListener("DOMContentLoaded", function () {
   const mainContainer = document.querySelector('.main-container');
   const resetButton = document.querySelector('#reset-button');
   const eraserButton = document.querySelector('#eraser-button');
-  let isErasing = false;
+  const colorModeButton = document.querySelector('#color-mode-button');
+  let isErasing = false; // eraser mode toggle
+  let isDrawingEnabled = true; // DEFAULTS to this mode
 
 
-  for (let i = 0; i < 16; i++) { // for looping 'i' for rows, 'j' for columns
+  function toggleDrawingEnabled () {
+    isDrawingEnabled = !isDrawingEnabled;
+    mainContainer.classList.toggle('drawing-enabled', !isDrawingEnabled);
+
+    if (isDrawingEnabled) {
+      colorModeButton.textContent = 'Color Mode';
+    } else {
+      colorModeButton.textContent = 'Drawing Disabled';
+    }
+  }
+
+  let isDrawing = false;
+
+  mainContainer.addEventListener('mousedown', function () {
+    isDrawing = isDrawingEnabled; // only enable drawing if it's allowed
+  });
+
+  mainContainer.addEventListener('mouseup', function () {
+    isDrawing = false;
+  });
+
+  mainContainer.addEventListener('mouseleave', function () {
+    isDrawing = false;
+  });
+
+  mainContainer.addEventListener('mousemove', function (event) {
+    if (isDrawing) {
+      const gridItem = event.target;
+      if (isErasing) {
+        gridItem.style.backgroundColor = 'white';
+      } else {
+        gridItem.style.backgroundColor = 'black';
+      }
+    }
+});
+
+  colorModeButton.addEventListener('click', function () {
+    toggleDrawingEnabled();
+  });
+
+  // ERASER BUTTON
+  eraserButton.addEventListener('click', function () {
+    isErasing = !isErasing; // toggle eraser mode immediately
+  });
+
+  for (let i = 0; i < 16; i++) { // for looping 'i' for rows, 'j' for columns; 16x16 by default
     for (let j = 0; j < 16; j++) {
       const gridItem = document.createElement('div');
       gridItem.classList.add('grid-item');
       mainContainer.appendChild(gridItem);
 
-      gridItem.addEventListener('mouseenter', function () {
-        if (isErasing) {
-          gridItem.style.backgroundColor = 'gray'; // erase by setting to gray
-        } else {
-          gridItem.style.backgroundColor = 'pink';
+      gridItem.addEventListener('mousedown', function () {
+        if (isDrawingEnabled) {
+          isDrawing = true;
+          const gridItem = event.target;
+          if (isErasing) {
+            gridItem.style.backgroundColor = 'white';
+          } else {
+          gridItem.style.backgroundColor = 'black';
         }
-      });
-    }
+      }
+    });
+
+    gridItem.addEventListener('mouseenter', function (event) {
+      if (isErasing && isDrawingEnabled) {
+        const gridItem = event.target;
+        gridItem.style.backgroundColor = 'white';
+      }
+    });
   }
+}
+  // RESET BUTTON
   resetButton.addEventListener('click', function () {
+    isErasing = false;
     const gridItems = mainContainer.querySelectorAll('.grid-item');
     gridItems.forEach(function (gridItem) {
-      gridItem.style.backgroundColor = 'gray';
+      gridItem.style.backgroundColor = 'white';
     });
-  });
-  eraserButton.addEventListener('click', function () {
-    isErasing = !isErasing; // toggle eraser mode
+    if (!isDrawingEnabled) {
+      toggleDrawingEnabled(); // enable drawing after reset
+    }
   });
 });
