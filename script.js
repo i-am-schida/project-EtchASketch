3bef7442785e344d77cd1ef1a1725f6dc2071b7f
Sam Ichida <84211012+i-am-schida@users.noreply.github.com>
2023-10-07 16:12:42 -0700

    additions: color wheel, css styling, drag-slider integration
    css styling mainly on buttons and footer. slider integration w grid size, color wheel addition to change color of pen

--- script.js
+++ script.js
@@ -7,6 +7,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const colSizeSlider = document.querySelector('#col-size');
   const rowSizeValue = document.querySelector('#row-size-value');
   const colSizeValue = document.querySelector('#col-size-value');
+  const colorWheel = document.querySelector('.color-wheel');
+  const selectedColor = document.querySelector('.selected-color');
 
   let isErasing = false;
   let isDrawingEnabled = true;
@@ -14,6 +16,37 @@ document.addEventListener("DOMContentLoaded", function () {
   let currentColSize = 16;
   let isDrawing = false;
   let isRainbowMode = false;
+  let selectedColorValue = 'black';
+
+  colorWheel.addEventListener('click', (event) => { // finds where color wheel is, then where i clicked on wheel in relation to the center of the wheel
+    const boundingBox = colorWheel.getBoundingClientRect(); 
+    const centerX = boundingBox.left + boundingBox.width / 2;
+    const centerY = boundingBox.top + boundingBox.height / 2;
+    const deltaX = event.clientX - centerX;
+    const deltaY = event.clientY - centerY;
+  
+    // Calculate the angle in radians
+    let angle = Math.atan2(deltaY, deltaX);
+  
+    // Adjust for the angle being measured clockwise from the positive x-axis
+    angle += Math.PI / 2;
+  
+    // Convert the angle to degrees and ensure it's within the 0-360 degree range
+    const degrees = ((angle * 180 / Math.PI + 360) % 360).toFixed(2);
+  
+    // Set the hue value and update the selected color
+    selectedColor.style.backgroundColor = `hsl(${degrees}, 100%, 50%)`;
+    selectedColorValue = `hsl(${degrees}, 100%, 50%)`;
+    isDrawing = false;
+  });
+
+  colorModeButton.addEventListener('click', () => {
+    if (colorModeButton.textContent === 'Color Mode') {  // Toggle between "Color Mode" and "Rainbow Mode"
+      colorModeButton.textContent = 'Rainbow Mode';
+    } else {
+      colorModeButton.textContent = 'Color Mode';
+    }
+  });
 
   function toggleDrawingEnabled() {
     isDrawingEnabled = !isDrawingEnabled;
@@ -29,8 +62,8 @@ document.addEventListener("DOMContentLoaded", function () {
   
   function createGrid() {
     mainContainer.innerHTML = '';
-    mainContainer.style.gridTemplateColumns = `repeat(${currentColSize}, 1fr)`;
-    mainContainer.style.gridTemplateRows = `repeat(${currentRowSize}, 1fr)`;
+    mainContainer.style.gridTemplateColumns = `repeat(${currentRowSize}, 1fr)`;
+    mainContainer.style.gridTemplateRows = `repeat(${currentColSize}, 1fr)`;
 
     for (let i = 0; i < currentRowSize * currentColSize; i++) {
       const gridItem = document.createElement('div');
@@ -49,7 +82,7 @@ document.addEventListener("DOMContentLoaded", function () {
           const randomColor = `rgb(${Math.random() * 255}, ${Math.random() * 255}, ${Math.random() * 255})`;
           gridItem.style.backgroundColor = randomColor;
         } else {
-          gridItem.style.backgroundColor = 'black';
+          gridItem.style.backgroundColor = selectedColorValue;
         }
       }
     }
@@ -68,6 +101,9 @@ document.addEventListener("DOMContentLoaded", function () {
     if (!isDrawingEnabled) {
       toggleDrawingEnabled();
     }
+
+    selectedColor.style.backgroundColor = 'black';
+    selectedColorValue = 'black';
   });
 
   eraserButton.addEventListener('click', function () {
